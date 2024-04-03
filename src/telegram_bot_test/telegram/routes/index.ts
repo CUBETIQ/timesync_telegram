e11ce3a12928ff761f1bcb8a8ab2137511d3257d
kit <kit@cubetiqs.com>
2024-04-03 10:08:05 +0700

    update

--- src/telegram_bot_test/telegram/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { getAllCommands, sendCommand } from "../controller/telegramController"
-
-const router: Router = Router()
-
-router.get("/commands", getAllCommands)
-router.post("/command", sendCommand)
-
-export default router

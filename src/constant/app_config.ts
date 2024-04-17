6819c82963ab31c28a9c9559c064456e840ad7cf
kit <kit@cubetiqs.com>
2024-04-17 13:41:35 +0700

    update

--- src/constant/app_config.ts
+++ src/constant/app_config.ts
@@ -4,4 +4,5 @@ dotenv.config()
 
 export const PORT = process.env.PORT || 3000
 
-export const REDIS_URL = process.env.REDIS_URL || 'redis://192.168.3.116:6379'
+export const REDIS_URL = process.env.REDIS_URL || "redis://192.168.3.116:6379"
+export const REDIS_LOCAL = process.env.REDIS_LOCAL
--- src/service/redisService.ts
+++ src/service/redisService.ts
@@ -1,17 +1,24 @@
-import { createClient } from "redis";
-import dotenv from "dotenv";
-import { handleRedisMessage } from "../controller/index";
-import { REDIS_URL } from "../constant/app_config";
+import { createClient } from "redis"
+import dotenv from "dotenv"
+import { handleRedisMessage } from "../controller/index"
+import { REDIS_LOCAL, REDIS_URL } from "../constant/app_config"
 
-dotenv.config();
+dotenv.config()
 
-const redisClient = createClient({ url: REDIS_URL });
+const redisClient = createClient({ url: REDIS_URL })
 
 const initRedis = async () => {
-  await redisClient.connect();
-  console.log("Redis connected successfully!");
+  try {
+    await redisClient.connect()
+    console.log("Redis connected successfully!")
+  } catch (error) {
+    redisClient.quit()
+    redisClient.options.url = REDIS_LOCAL
+    await redisClient.connect()
+    console.log("Connected to Redis using default URL")
+  }
 
-  redisClient.subscribe("attendance.created", handleRedisMessage);
-};
+  redisClient.subscribe("attendance.created", handleRedisMessage)
+}
 
-export { initRedis };
+export { initRedis }
